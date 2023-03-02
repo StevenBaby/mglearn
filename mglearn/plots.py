84a5b4834515704f40eb817f825f8d43802bff93
Steven Kang <kangweibaby@163.com>
2023-03-02 18:46:12 +0800

    02.3 Linear

--- mglearn/plots.py
+++ mglearn/plots.py
@@ -1,5 +1,13 @@
+from .plot_2d_separator import plot_2d_separator
+from .plot_2d_separator import plot_2d_classification
+
+from .plot_helpers import cm2, cm3
 
 from .plot_knn_classification import plot_knn_classification
 from .plot_knn_regression import plot_knn_regression
-from .plot_2d_separator import plot_2d_separator
-from .plot_helpers import cm2, cm3
+
+from .plot_linear_regression import plot_linear_regression_wave
+
+from .plot_ridge import plot_ridge_n_samples
+
+from .plot_linear_svc_regularization import plot_linear_svc_regularization
