66f49c31004b1a1b2f414346b4556031b0d5c888
Steven Kang <kangweibaby@163.com>
2023-03-02 18:33:15 +0800

    update datasets

--- mglearn/datasets.py
+++ mglearn/datasets.py
@@ -1,6 +1,11 @@
 
+import os
 from sklearn import datasets
+
 import numpy as np
+import pandas as pd
+
+DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
 
 
 def make_forge():
@@ -19,3 +24,46 @@ def make_wave(n_samples=100):
     y_no_noise = (np.sin(4 * x) + x)
     y = (y_no_noise + rnd.normal(size=len(x))) / 2
     return x.reshape(-1, 1), y
+
+
+def load_boston(*, return_X_y=False):
+    import csv
+    from sklearn.utils import Bunch
+    filename = os.path.join(DATA_FOLDER, 'boston_house_prices.csv')
+    with open(filename) as f:
+        data_file = csv.reader(f)
+
+        temp = next(data_file)
+        n_samples = int(temp[0])
+        n_features = int(temp[1])
+        data = np.empty((n_samples, n_features))
+        target = np.empty((n_samples,))
+        temp = next(data_file)  # names of features
+        feature_names = np.array(temp)
+
+        for i, d in enumerate(data_file):
+            data[i] = np.asarray(d[:-1], dtype=np.float64)
+            target[i] = np.asarray(d[-1], dtype=np.float64)
+
+    if return_X_y:
+        return data, target
+
+    return Bunch(
+        data=data,
+        target=target,
+        # last column is target value
+        feature_names=feature_names[:-1],
+        DESCR='',
+        filename=filename,
+    )
+
+
+def load_extended_boston():
+    from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
+
+    boston = load_boston()
+    X = boston.data
+
+    X = MinMaxScaler().fit_transform(boston.data)
+    X = PolynomialFeatures(degree=2, include_bias=False).fit_transform(X)
+    return X, boston.target
